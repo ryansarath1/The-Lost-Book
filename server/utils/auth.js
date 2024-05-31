3f1b1d120ae5b9d40fb8ae2c04e37631ec9ca94f
Ryan945 <sarathryan@yahoo.com>
2024-05-31 16:43:26 -0700

    fixed more merge conflicts

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,16 +1,14 @@
 const jwt = require('jsonwebtoken');
-<<<<<<< HEAD
 const {GraphQLError} = require("graphql")
-=======
 
->>>>>>> 516a53b493dcc966a3f8297a860dcd3c2d9657eb
+
+
 // set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-<<<<<<< HEAD
   AuthenticationError: new GraphQLError("can't authenticate user", {
     extensions:{
       code: "UNAUTHENTICATED"
@@ -19,11 +17,7 @@ module.exports = {
   authMiddleware: function (req, ) {
     // allows token to be sent via  req.query or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
-=======
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
->>>>>>> 516a53b493dcc966a3f8297a860dcd3c2d9657eb
+
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -31,11 +25,9 @@ module.exports = {
     }
 
     if (!token) {
-<<<<<<< HEAD
       return req
-=======
-      return res.status(400).json({ message: 'You have no token!' });
->>>>>>> 516a53b493dcc966a3f8297a860dcd3c2d9657eb
+
+      
     }
 
     // verify token and get user data out of it
@@ -44,19 +36,13 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-<<<<<<< HEAD
      
     }
 
     // send to next endpoint
     return req
-=======
-      return res.status(400).json({ message: 'invalid token!' });
-    }
+     // send to next endpoint
 
-    // send to next endpoint
-    next();
->>>>>>> 516a53b493dcc966a3f8297a860dcd3c2d9657eb
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
