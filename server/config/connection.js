87333a71b97d45bed093a172fdf43d969545f9ba
Ryan945 <sarathryan@yahoo.com>
2024-06-06 21:46:34 -0700

    please work

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-const mongoURI = process.env.MONGO_URI || 'mongodb+srv://ryan:Winners123@cluster0.mongodb.net/<dbname>?retryWrites=true&w=majority';
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+module.exports = mongoose.connection;
